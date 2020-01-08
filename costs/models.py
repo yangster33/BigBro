a189721c088548ece489ee3c5c61f8196f8ec3cc
yangster <yangster33@163.com>
2020-01-08 13:02:34 +0800

    first update

---
+++ costs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class MyUser(AbstractUser):
+
+    position = models.CharField(verbose_name='岗位', max_length=50, default='员工')
+    avatar = models.ImageField(
+        verbose_name='头像', upload_to="avatars", default='avatars/default.jpg',)
+
+    diligent_num = models.IntegerField(verbose_name='勤奋指数', default=80)
+
+    legend = models.IntegerField(verbose_name='传说事迹', default=0)
+    epic = models.IntegerField(verbose_name='史诗行为', default=0)
+    rare = models.IntegerField(verbose_name='稀有业绩', default=0)
+
+    def __str__(self):
+        return self.username
+
+    def delete(self, *args, **kwargs):
+        path = self.avatar.path
+        if path:
+            os.remove(path)
+        super(CustomUser, self).delete(*args, **kwargs)
