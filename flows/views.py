d57d274e3053ed23e8e8e9862bd49e7dff85b068
yangster <yangster33@163.com>
2020-01-23 22:03:02 +0800

    flows

--- flows/views.py
+++ flows/views.py
@@ -27,6 +27,12 @@ class FlowsView(ArchivesMixin, ListView):
         '巫山', '巫溪', '石柱',
         '秀山', '酉阳', '彭水',
     ]
+    model_fields = [
+        'location', 'work', 'trans_cost',
+        'hotel_cost', 'local_trans_cost',
+        'meat_cost', 'local_car_cost', 
+        'other_cost_1',
+    ]
 
     def get_queryset(self):
         qs = Costs.objects.all()
@@ -47,10 +53,20 @@ class FlowsView(ArchivesMixin, ListView):
         return context
 
     def post(self, requset, *arg, **kargs):
-    
-        
-        print(self.request.POST)
-        print(self.request)
+        update_post = self.request.POST
+        qs = Costs.objects.filter(account=self.request.user)
+        for i in range(1,6):
+            now_qs = qs.get(travel_date=update_post['date '+str(i)])
+            for m in self.model_fields:
+                set_data = update_post[m+' '+str(i)]
+                if m[-4:] == 'cost' or m[-6:-2] == 'cost':
+                    if set_data == '':
+                        set_data = 0
+                    else:
+                        set_data = float(set_data)
+                setattr(now_qs, m, set_data)
+            now_qs.save()
+            
 
         return redirect('flows')
 
