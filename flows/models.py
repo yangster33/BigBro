883772046067381ff0cf1fa5ae75bb2187fe657e
yangster <yangster33@163.com>
2020-01-29 10:46:32 +0800

    flows

--- flows/models.py
+++ flows/models.py
@@ -17,3 +17,21 @@ class WeekendCostsFlows(models.Model):
     
     def __str__(self):
         return self.name
+
+class TempFlows(models.Model):
+
+    name = models.CharField(verbose_name='流程名称', max_length=50)
+    desc = models.CharField(verbose_name='流程描述', max_length=200)
+    model = models.CharField(verbose_name='关联model', max_length=50)
+    data = models.CharField(verbose_name='数据', max_length=1000)
+    flow = models.CharField(verbose_name='流程内容', max_length=200)
+    step = models.IntegerField(verbose_name='当前流程状态', default=0)
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    done_time = models.DateTimeField(verbose_name='完成时间')
+
+    class Meta:
+        verbose_name = '临时流程'
+        verbose_name_plural = '临时流程'
+    
+    def __str__(self):
+        return self.name
