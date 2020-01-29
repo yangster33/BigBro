25a848476d73f42682de029a1438f9c9993e05c8
yangster <yangster33@163.com>
2020-01-29 11:13:26 +0800

    flows

--- flows/middleware.py
+++ flows/middleware.py
@@ -2,23 +2,38 @@ from django.shortcuts import render, redirect
 
 
 from costs.models import Costs
+from flows.models import TempFlows
+import datetime
+
+# class WeekendUpdateMixin:
+
+#     def post(self, requset, *arg, **kargs):
+#         qs = Costs.objects.filter(account=self.requset.user)
+#         update_post = self.requset.POST
+#         for i in range(1, 7):
+#             now_qs = qs.get(travel_date=update_post['date '+str(i)])
+#             for m in self.model_fields:
+#                 set_data = update_post[m+' '+str(i)]
+#                 if m[-4:] == 'cost' or m[-6:-2] == 'cost':
+#                     if set_data == '':
+#                         set_data = 0
+#                     else:
+#                         set_data = float(set_data)
+#                 setattr(now_qs, m, set_data)
+#             now_qs.save()
+
+#         return redirect('flows')
 
 
-class WeekendUpdateMixin:
-
-    def post(self, requset, *arg, **kargs):
-        qs = Costs.objects.filter(account=self.requset.user)
-        update_post = self.requset.POST
-        for i in range(1, 7):
-            now_qs = qs.get(travel_date=update_post['date '+str(i)])
-            for m in self.model_fields:
-                set_data = update_post[m+' '+str(i)]
-                if m[-4:] == 'cost' or m[-6:-2] == 'cost':
-                    if set_data == '':
-                        set_data = 0
-                    else:
-                        set_data = float(set_data)
-                setattr(now_qs, m, set_data)
-            now_qs.save()
-
-        return redirect('flows')
+def TempFlowsCreater(username, model, data, flows, step=0):
+    now = datetime.datetime.now()
+    done_time = datetime.datetime(year=1900, month=1, day=1)
+    TempFlows.objects.create(
+        name = now+model.__name__+username,
+        desc = 'tempflow',
+        model = model.__name__,
+        data = data,
+        flow = flows,
+        step = step,
+        done_time = done_time,
+    )
